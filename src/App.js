6492c49e754aa851bb87dfe7da07ab3f5486133a
DDing77 <smg76262585@gmail.com>
2021-04-16 04:16:12 +0900

    1-12

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import Hello from './Hello';
 import Wrapper from './Wrapper';
 import Counter from './Counter';
@@ -6,6 +6,33 @@ import InputSample from './InputSample';
 import UserList from './UserList';
 import './App.css';
 function App() {
+
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  }
+
   return (
     <Wrapper>
       <Hello name="react" color="red" isSpecial={true}/>
@@ -13,7 +40,7 @@ function App() {
       <Counter/>
       <InputSample/>
       <br/>
-      <UserList/>
+      <UserList users={users} />
     </Wrapper>
     //adfafd
   
