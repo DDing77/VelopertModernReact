4612db425a2e1db3771604d4952bd5b818feb3e7
DDing77 <smg76262585@gmail.com>
2021-10-03 19:01:17 +0900

    1-12

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Counter from './Counter';
 import InputSample from './InputSample';
 import UserList from './UserList';
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    nextId.current += 1;
+  };
+
+  console.log(nextId.current);
+
   return (
     <>
-    <Counter/>
-    <InputSample/>
-    <UserList/>
+      <Counter />
+      <InputSample />
+      <UserList users={users} />;
     </>
   );
 }
