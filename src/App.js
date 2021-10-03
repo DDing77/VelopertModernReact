62c8c4573e6f5328250f3002bc27b0d1bb21f1f2
DDing77 <smg76262585@gmail.com>
2021-10-03 18:44:32 +0900

    1-11

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Counter from './Counter';
 import InputSample from './InputSample';
+import UserList from './UserList';
 
 function App() {
   return (
     <>
     <Counter/>
     <InputSample/>
+    <UserList/>
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,45 +1,38 @@
-import React , { useEffect } from 'react';
+import React from 'react';
 
-function User({ user, onRemove, onToggle }) {
-
-  useEffect(() => {
-    console.log('user 값이 설정됨');
-    console.log(user);
-    return () => {
-      console.log('user가 바뀌기 전..');
-      console.log(user);
-    }
-  }, [user]);
-
+function User({ user }) {
   return (
     <div>
-      <b
-        style={{
-          cursor: 'pointer',
-          color: user.active ? 'green' : 'black'
-        }}
-        onClick={() => onToggle(user.id)}
-      >
-        {user.username}
-      </b>
-      &nbsp;
-      <span>({user.email})</span>
-      <button onClick={() => onRemove(user.id)}>삭제</button>
+      <b>{user.username}</b> <span>({user.email})</span>
     </div>
   );
 }
 
-function UserList({ users, onRemove, onToggle }) {
+function UserList() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
   return (
     <div>
-      {users.map(user => (
-        <User
-          user={user}
-          key={user.id}
-          onRemove={onRemove}
-          onToggle={onToggle}
-        />
-      ))}
+      <div>
+        {users.map(user => (
+          <User user={user} key={user.id}/>
+        ))}
+      </div>
     </div>
   );
 }
